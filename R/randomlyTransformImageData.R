e3107855865ce7b0e45047c0550eab79adfd5025
ntustison <ntustison@gmail.com>
2018-08-14 12:51:32 -0700

    ENH:  Add second version of random transform image function.

---
+++ R/randomlyTransformImageData.R
@@ -0,0 +1,236 @@
+#' Randomly transform image data (optional: with corresponding segmentations). 
+#'
+#' Apply rigid, affine and/or deformable maps to an input set of training 
+#' images.  The reference image domain defines the space in which this happens.
+#'
+#' @param referenceImage defines the spatial domain for all output images.  If 
+#' the input images do not match the spatial domain of the reference image, we
+#' internally resample the target to the reference image.  This could have 
+#' unexpected consequences.  Resampling to the reference domain is performed by
+#' testing using \code{antsImagePhysicalSpaceConsistency} then calling
+#' \code{resampleImageToTarget} upon failure.
+#' @param inputImageList list of lists of input images to warp.  The internal
+#' list sets contains one or more images (per subject) which are assumed to be
+#' mutually aligned.  The outer list contains multiple subject lists which are
+#' randomly sampled to produce output image list.  
+#' @param segmentationImageList list of segmentation images corresponding to the
+#' input image list (optional).
+#' @param numberOfSimulations number of output images.  Default = 10.
+#' @param transformType one of the following options 
+#' \code{c( "translation", "rigid", "scaleShear", "affine"," deformation" ,
+#'   "affineAndDeformation" )}.  Default = \"affine\".
+#' @param inputImageInterpolator one of the following options 
+#' \code{ c( "linear", "gaussian", "bspline" )}.  Default = \"linear\".
+#' @param segmentationImageInterpolator one of the following options 
+#' \code{ c( "nearestNeighbor", "genericLabel" )}.  Default = 
+#' \"nearestNeighbor\".
+#' @param numberOfControlPoints number of control points for simulated 
+#' deformations. Default = 100.
+#' @param sdAffine parameter dictating deviation amount from identity for 
+#' random linear transformations.  Default = 1.0.
+#' @param spatialSmoothing parameter dictating amount of spatial 
+#' smoothing.  Default = 3.0.
+#' @return list (if no directory set) or boolean for success, failure
+#' @author Avants BB
+#' @seealso \code{\link{randomImageTransformBatchGenerator}}
+#' @importFrom ANTsRCore getAntsrTransformFixedParameters iMath resampleImageToTarget applyAntsrTransform antsImagePhysicalSpaceConsistency antsrTransformFromDisplacementField makeImage smoothImage setAntsrTransformFixedParameters getAntsrTransformParameters setAntsrTransformParameters readAntsrTransform createAntsrTransform randomMask mergeChannels
+#' @importFrom ANTsR composeTransformsToField
+#' @importFrom stats rnorm
+#' @examples
+#'
+#' library( ANTsR )
+#' image1 <- antsImageRead( getANTsRData( "r16" ) )
+#' image2 <- antsImageRead( getANTsRData( "r64" ) )
+#' segmentation1 <- thresholdImage( i1, "Otsu", 3 )
+#' segmentation2 <- thresholdImage( i2, "Otsu", 3 )
+#' data <- randomlyTransformImageData( image1,
+#'   list( list( image1 ), list( image2 ) ),  
+#'   list( segmentation1, segmentation2 ) )
+#' @export randomlyTransformImageData
+
+randomlyTransformImageData <- function( referenceImage,
+  inputImageList, segmentationImageList = NA, numberOfSimulations = 10,
+  transformType = 'affine', inputImageInterpolator = 'linear',
+  segmentationImageInterpolator = 'genericLabel',
+  numberOfControlPoints = 100, sdAffine = 1.0, 
+  spatialSmoothing = 3.0 )
+{
+
+  polarDecomposition <- function( X )
+    {
+    svdX <- svd( X )
+    P <- svdX$u %*% diag( svdX$d ) %*% t( svdX$u )
+    Z <- svdX$u %*% t( svdX$v )
+    if ( det( Z ) < 0 ) 
+      {
+      Z <- Z * ( -1 )
+      }
+    return( list( P = P, Z = Z, Xtilde = P %*% Z ) )
+    }
+
+  createRandomLinearTransform <- function( 
+    image, fixedParameters, transformType = 'affine', sdAffine = 1.0 ) 
+    {
+    transform <- createAntsrTransform( precision = "float", 
+      type = "AffineTransform", dimension = image@dimension )
+    setAntsrTransformFixedParameters( transform, fixedParameters )
+    identityParameters <- getAntsrTransformParameters( transform )
+
+    randomEpsilon <- stats::rnorm( length( identityParameters ), mean = 0, 
+      sd = sdAffine )
+    if ( transformType == 'translation' )
+      {
+      randomEpsilon[1:( length( identityParameters ) - image@dimension )] <- 0
+      }
+
+    randomParameters <- identityParameters + randomEpsilon
+    randomMatrix <- matrix( randomParameters[
+      1:( length( randomParameters ) - image@dimension )], 
+        ncol = image@dimension )
+    decomposition <- polarDecomposition( randomMatrix )
+
+    if( transformType == "rigid" )
+      { 
+      randomMatrix <- decomposition$Z
+      }
+    if( transformType == "affine" ) 
+      {
+      randomMatrix <- decomposition$Xtilde
+      }
+    if( transformType == "scaleShear" ) 
+      {
+      randomMatrix <- decomposition$P
+      }
+
+    randomParameters[1:( length( identityParameters ) - image@dimension )] <- 
+      as.numeric( randomMatrix )
+    setAntsrTransformParameters( transform, randomParameters )
+    return( transform )
+    }
+
+  createRandomDisplacementFieldTransform <- function( image, 
+    numberOfControlPoints = 100, spatialSmoothing = 3.0, 
+    dilationRadius = 5, numberOfCompositions = 10, gradientStep = 0.1 )
+    {
+    maskImage <- image * 0.0 + 1.0
+
+    displacementFieldComponents <- list()
+    for( d in 1:image@dimension ) 
+      {
+      randomMaskForTransform <- gradientStep * 
+        randomMask( maskImage, numberOfControlPoints )
+      fieldComponent <- iMath( randomMaskForTransform, "MD", dilationRadius ) %>% 
+        smoothImage( spatialSmoothing )
+      displacementFieldComponents[[d]] <- fieldComponent
+      }
+    displacementField <- mergeChannels( displacementFieldComponents )
+
+    composedField <- NA
+    for ( i in 1:numberOfCompositions ) 
+      {
+      composedField <- composeTransformsToField( composedField, displacementField )
+      }
+    transform <- antsrTransformFromDisplacementField( composedField )
+
+    return( transform )
+    }
+
+
+  admissibleTransforms <- c( "translation", "rigid", "scaleShear", "affine", 
+    "affineAndDeformation", "deformation" )
+  if( !( transformType %in% admissibleTransforms ) )
+    {
+    stop( paste0( "The specified transform, ", transformType, 
+      "is not a possible option.  Please see help menu." ) )    
+    }
+
+  # Get the fixed parameters from the reference image.
+
+  referenceRegistration <- antsRegistration( fixedImage = referenceImage, 
+    movingImage = referenceImage, typeofTransform = 'Rigid', 
+    regIterations = c( 1, 0 ) )
+  referenceTransform <- readAntsrTransform( 
+    referenceRegistration$fwdtransforms[1], referenceImage@dimension )
+  fixedParameters = getAntsrTransformFixedParameters( referenceTransform )
+
+  numberOfSubjects <- length( inputImageList )   
+  numberOfImagesPerSubject <- length( inputImageList[[1]] )
+
+  randomIndices <- sample( numberOfSubjects, size = numberOfSimulations )
+
+  simulatedImageList <- list()
+  simulatedSegmentationImageList <- list()
+  for( i in seq_len( numberOfSimulations ) )
+    {
+    singleSubjectImageList <- inputImageList[[randomIndices[i]]] 
+    singleSubjectSegmentationImage <- NA
+    if( !is.na( segmentationImageList ) )
+      {
+      singleSubjectSegmentationImage <- segmentationImageList[[randomIndices[i]]]
+      }
+
+    if( !antsImagePhysicalSpaceConsistency( referenceImage, singleSubjectImageList[[1]] ) )
+      {
+      for( j in 1:length( singleSubjectImageList ) )
+        {
+        singleSubjectImageList[[j]] <- resampleImageToTarget( 
+          singleSubjectImageList[[j]], referenceImage, 
+            interpType = inputImageInterpolator )
+        }
+      if( !is.na( singleSubjectSegmentationImage ) )
+        {
+        singleSubjectSegmentationImage <- resampleImageToTarget( 
+          singleSubjectSegmentationImage, referenceImage,
+            interpType = segmentationImageInterpolator )
+        }    
+      }
+
+    transforms <- c()
+    if( typeOfTransform == 'Deformation' )
+      {
+      deformableTransform <- createRandomDisplacementFieldTransform(     
+        referenceImage, numberOfControlPoints, spatialSmoothing )
+      transforms <- c( deformableTransform )
+      }  
+    if( typeOfTransform == 'AffineAndDeformation' )
+      {
+      deformableTransform <- createRandomDisplacementFieldTransform(     
+        referenceImage, numberOfControlPoints, spatialSmoothing )
+      linearTransform <- createRandomLinearTransform( referenceImage, 
+        fixedParameters, 'affine', sdAffine )  
+      transforms <- c( deformableTransform, linearTransform )
+      }  
+    if( typeOfTransform %in% admissibleTransforms[1:4] )
+      {
+      linearTransform <- createRandomLinearTransform( referenceImage, 
+        fixedParameters, typeOfTransform, sdAffine )
+      transforms <- c( linearTransform )
+      }
+
+    singleSubjectSimulatedImageList <- list()
+    for( j in 1:length( singleSubjectImageList ) )
+      {
+      singleSubjectSimulatedImageList[[j]] <- applyAntsrTransform( transforms, 
+        singleSubjectImageList[[j]], referenceImage, 
+          interpolation = inputImageInterpolator )
+      }
+    simulatedImageList[[i]] <- singleSubjectSimulatedImageList
+
+    if( !is.na( singleSubjectSegmentationImage ) )
+      {
+      simulatedSegmentationImageList[[i]] <- applyAntsrTransform( transforms, 
+        singleSubjectSegmentationImage, referenceImage, 
+          interpolation = segmentationImageInterpolator )
+      }    
+    }
+
+  if( is.na( segmentationImageList ) )
+    {
+    return( list( simulatedImageList = simulatedImageList ) )  
+    }
+  else 
+    {
+    return( list( simulatedImageList = simulatedImageList, 
+      simulatedSegmentationImageList = simulatedSegmentationImageList ) )  
+    }
+}
