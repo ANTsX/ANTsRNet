845024d8ffa104b146a4b97ff16fa72bca0c65e1
Nick Tustison <ntustison@gmail.com>
2018-12-13 15:18:53 -0800

    ENH:  Add another mdoel.

--- R/getPretrainedNetwork.R
+++ R/getPretrainedNetwork.R
@@ -22,7 +22,7 @@ getPretrainedNetwork <- function(fileid,
     print(myusage)
     return(NULL)
   }
-  validlist = c( "dbpn4x", "brainExtraction", "brainSegmentation", "protonLungMri", "show" )
+  validlist = c( "dbpn4x", "brainExtraction", "brainSegmentation", "protonLungMri", "functionalLungMri", "show" )
   if (  sum( validlist == fileid ) == 0 ) {
     message("Try:")
     print( validlist )
@@ -34,7 +34,8 @@ getPretrainedNetwork <- function(fileid,
                    dbpn4x = "https://ndownloader.figshare.com/files/13347617",
                    brainExtraction = "https://ndownloader.figshare.com/files/13729661",
                    brainSegmentation = "https://ndownloader.figshare.com/files/13812053",
-                   protonLungMri = "https://ndownloader.figshare.com/files/13606799"
+                   protonLungMri = "https://ndownloader.figshare.com/files/13606799",
+                   functionalLungMri = "https://ndownloader.figshare.com/files/13824167"
                    )
 
   if ( missing( targetFileName ) ) {
