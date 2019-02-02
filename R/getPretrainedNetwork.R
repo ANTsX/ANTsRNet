bdff03fba93dceae68cc7b2d7d1e42d7e2e0c915
Nick Tustison <ntustison@gmail.com>
2019-02-01 21:14:59 -0800

    ENH:  Add denoising weights.

--- R/getPretrainedNetwork.R
+++ R/getPretrainedNetwork.R
@@ -22,7 +22,8 @@ getPretrainedNetwork <- function(fileid,
     print(myusage)
     return(NULL)
   }
-  validlist = c( "dbpn4x", "brainExtraction", "brainSegmentation", "wholeTumorSegmentationT2Flair", "protonLungMri", "functionalLungMri", "show" )
+  validlist = c( "dbpn4x", "brainExtraction", "brainSegmentation", "denoising",
+                 "wholeTumorSegmentationT2Flair", "protonLungMri", "functionalLungMri", "show" )
   if (  sum( validlist == fileid ) == 0 ) {
     message("Try:")
     print( validlist )
@@ -34,6 +35,7 @@ getPretrainedNetwork <- function(fileid,
                    dbpn4x = "https://ndownloader.figshare.com/files/13347617",
                    brainExtraction = "https://ndownloader.figshare.com/files/13729661",
                    brainSegmentation = "https://ndownloader.figshare.com/files/13900010",
+                   denoising = "https://ndownloader.figshare.com/files/14235296",
                    wholeTumorSegmentationT2Flair = "https://ndownloader.figshare.com/files/14087045",
                    protonLungMri = "https://ndownloader.figshare.com/files/13606799",
                    functionalLungMri = "https://ndownloader.figshare.com/files/13824167"
