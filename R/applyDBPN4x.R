102061db3eccd19c267cf3374e1b527ba5d61e45
stnava <stnava@gmail.com>
2019-06-26 12:36:39 -0400

    BUG: slice array

--- R/applyDBPN4x.R
+++ R/applyDBPN4x.R
@@ -206,13 +206,13 @@ if ( channelSize != ncomponents ) {
 
 sliceArray <- function(  myArr, j ) {
   if ( shapeLength == 3 ) {
-    return( myArr[1,,j] )
+    return( myArr[j,,] )
   }
   if ( shapeLength == 4 ) {
-    return( myArr[1,,,j] )
+    return( myArr[j,,,] )
   }
   if ( shapeLength == 5 ) {
-    return( myArr[1,,,,j] )
+    return( myArr[j,,,,] )
   }
 }
 ###############
