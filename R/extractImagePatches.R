7a8e445d0ec0680061a9599470794fdbe4c7ec7d
stnava <stnava@gmail.com>
2019-08-01 14:36:56 -0400

    ENH: return patch coordinates

--- R/extractImagePatches.R
+++ R/extractImagePatches.R
@@ -302,3 +302,204 @@ extractImagePatches <- function( image, patchSize, maxNumberOfPatches = 'all',
     return( patchList )
     }
 }
+
+
+#' Extract 2-D or 3-D image patch coordinates.
+#'
+#' @param image Input ANTs image with one or more components.
+#' @param patchSize Width, height, and depth (if 3-D) of patches.
+#' @param maxNumberOfPatches Maximum number of patches returned.  If
+#' "all" is specified, then all patches in sequence (defined by the
+#' \code{strideLength} are extracted.
+#' @param strideLength Defines the sequential patch overlap for
+#' \code{maxNumberOfPatches = "all"}.  Can be a image-dimensional vector or a scalar.
+#' @param maskImage optional image specifying the sampling region for
+#' the patches when \code{maximumNumberOfPatches} does not equal "all".
+#' The way we constrain patch selection using a mask is by forcing
+#' each returned patch to have a masked voxel at its center.
+#' @param physicalCoordinates boolean to determine whether indices or spatial
+#' coordinates are returned.
+#' @param randomSeed integer seed that allows reproducible patch extraction
+#' across runs.
+#'
+#' @return a matrix of image patch spatial or index coordinates.
+#' @author Tustison NJ, Avants B
+#' @examples
+#'
+#' library( ANTsR )
+#'
+#' image <- antsImageRead( getANTsRData( "r16" ) )
+#' maskImage <- getMask( image, 1, 1000 )
+#' patchCoordsP <- extractImagePatchCoordinates( image, c( 32, 32 ), 10, c( 32, 32 ),
+#'   maskImage, randomSeed = 0, physicalCoordinates = TRUE )
+#' patchCoordsI <- extractImagePatchCoordinates( image, c( 32, 32 ), 10, c( 32, 32 ),
+#'   maskImage, randomSeed = 0, physicalCoordinates = FALSE )
+#'
+#' @export
+extractImagePatchCoordinates <- function( image, patchSize, maxNumberOfPatches = 'all',
+  strideLength = 1, maskImage = NULL, physicalCoordinates = TRUE,
+  randomSeed )
+{
+
+  indexList = list()
+  toPhysical <- function( inds, physical ) {
+    if ( is.list( inds ) ) {
+      ptmat = matrix( nrow = length( inds ), ncol = image@dimension )
+      for ( k in 1:length( inds ) ) {
+        ptmat[k,] = antsTransformIndexToPhysicalPoint( image, inds[[k]] )
+      }
+      if ( physical ) return( antsTransformIndexToPhysicalPoint( image, ptmat ) )
+      if ( !physical ) return( ptmat )
+    }
+    if ( is.array( inds ) ) {
+      ptmat = matrix( nrow = length( inds ), ncol = image@dimension )
+      for ( k in 1:nrow( inds ) ) {
+        ptmat[k,] = antsTransformIndexToPhysicalPoint( image, inds[k,] )
+      }
+      ptmat=ptmat[ !is.na( ptmat[,1] ),]
+      if ( physical ) return( antsTransformIndexToPhysicalPoint( image, ptmat ) )
+      if ( !physical ) return( ptmat )
+    }
+    stop( "Index class unknown" )
+  }
+
+  if ( ! missing( randomSeed ) )
+    {
+    set.seed( randomSeed )
+    }
+  imageSize <- dim( image )
+  dimensionality <- length( imageSize )
+
+  if( dimensionality != 2 && dimensionality != 3 )
+    {
+    stop( "Unsupported dimensionality." )
+    }
+
+  numberOfImageComponents <- image@components
+
+  if( length( imageSize ) != length( patchSize ) )
+    {
+    stop( "Mismatch between the image size and the specified patch size." )
+    }
+  if( any( patchSize > imageSize ) )
+    {
+    stop( "Patch size is greater than the image size.")
+    }
+
+  patchList <- list()
+  midPatchIndex <- round( patchSize / 2 )
+
+  numberOfExtractedPatches <- maxNumberOfPatches
+
+  if( tolower( maxNumberOfPatches ) == 'all' )
+    {
+    strideLengthVector <- strideLength
+    if( length( strideLength ) == 1 )
+      {
+      strideLengthVector <- rep.int( strideLength, dimensionality )
+      } else if( length( strideLength ) != dimensionality ) {
+      stop( paste0( "strideLength is not a scalar or vector of
+        length dimensionality." ) )
+      } else if( any( strideLength < 1 ) ) {
+      stop( paste0( "strideLength elements must be positive integers." ) )
+      }
+
+    numberOfExtractedPatches <- 1
+
+    indices <- list()
+    for( d in seq_len( dimensionality ) )
+      {
+      indices[[d]] <- seq.int( from = 1, to = imageSize[d] - patchSize[d] + 1,
+        by = strideLengthVector[d] )
+      numberOfExtractedPatches <-
+        numberOfExtractedPatches * length( indices[[d]] )
+      }
+    count <- 1
+    if( dimensionality == 2 )
+      {
+      for( i in indices[[1]] )
+        {
+        for( j in indices[[2]] )
+          {
+          startIndex <- c( i, j )
+          endIndex <- startIndex + patchSize - 1
+          indexList[[ count ]] = c( i, j ) + midPatchIndex
+          count <- count + 1
+          }
+        }
+      } else {
+      for( i in indices[[1]] )
+        {
+        for( j in indices[[2]] )
+          {
+          for( k in indices[[3]] )
+            {
+            startIndex <- c( i, j, k )
+            endIndex <- startIndex + patchSize - 1
+            indexList[[ count ]] = c( i, j, k ) + midPatchIndex
+            count <- count + 1
+            }
+          }
+        }
+      }
+      return( toPhysical( indexList, physicalCoordinates ) )
+    } else {
+
+    randomIndices <-
+      array( data = NA, dim = c( maxNumberOfPatches, dimensionality ) )
+
+    if( !is.null( maskImage ) )
+      {
+      maskArray <- as.array( maskImage )
+      maskArray[which( maskArray != 0 )] <- 1
+
+      # The way we constrain patch selection using a mask is by assuming that
+      # each patch must have a masked voxel at its midPatchIndex.
+
+      maskIndices <- which( maskArray != 0, arr.ind = TRUE )
+
+      shiftedMaskIndices <- maskIndices
+      removeIndices <- c()
+      for( d in seq_len( dimensionality ) )
+        {
+        shiftedMaskIndices[, d] <- maskIndices[, d] - midPatchIndex[d]
+        removeIndices <- append( removeIndices, which( shiftedMaskIndices[, d] <= 0 ) )
+        shiftedMaskIndices[, d] <- maskIndices[, d] + midPatchIndex[d]
+        removeIndices <- append( removeIndices, which( shiftedMaskIndices[, d] > imageSize[d] ) )
+        }
+
+      removeIndices <- unique( removeIndices )
+      if( length( removeIndices ) > 0 )
+        {
+        maskIndices <- maskIndices[-removeIndices,]
+        }
+
+      # After pruning the mask indices, which were originally defined in terms of the
+      # midPatchIndex, we subtract the midPatchIndex so that it's now defined at the
+      # corner for patch selection.
+
+      for( d in seq_len( dimensionality ) )
+        {
+        maskIndices[, d] <- maskIndices[, d] - midPatchIndex[d]
+        }
+
+      numberOfExtractedPatches <- min( maxNumberOfPatches, nrow( maskIndices ) )
+
+      randomIndices <- maskIndices[
+        sample.int( nrow( maskIndices ), numberOfExtractedPatches ),]
+      } else {
+      for( d in seq_len( dimensionality ) )
+        {
+        randomIndices[, d] <- sample.int(
+          imageSize[d] - patchSize[d] + 1, maxNumberOfPatches, replace = TRUE )
+        }
+      }
+
+
+      for( d in seq_len( dimensionality ) )
+        {
+        randomIndices[, d] <- randomIndices[, d] + midPatchIndex[d]
+        }
+      return( toPhysical( randomIndices, physicalCoordinates ) )
+    }
+}
