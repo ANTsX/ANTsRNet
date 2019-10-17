424e4cd59a1c882c00594df98d67d4e53e9acb81
Nick Tustison <ntustison@gmail.com>
2019-10-17 11:01:16 -0700

    ENH:  Add no brainer u-net model.

--- R/createUnetModel.R
+++ R/createUnetModel.R
@@ -116,7 +116,7 @@
 #' @import keras
 #' @export
 createUnetModel2D <- function( inputImageSize,
-                               numberOfOutputs = 1,
+                               numberOfOutputs = 2,
                                numberOfLayers = 4,
                                numberOfFiltersAtBaseLayer = 32,
                                convolutionKernelSize = c( 3, 3 ),
@@ -226,7 +226,6 @@ createUnetModel2D <- function( inputImageSize,
   return( unetModel )
 }
 
-
 #' 3-D image segmentation implementation of the U-net deep learning architecture.
 #'
 #' Creates a keras model of the U-net deep learning architecture for image
@@ -293,7 +292,7 @@ createUnetModel2D <- function( inputImageSize,
 #' @import keras
 #' @export
 createUnetModel3D <- function( inputImageSize,
-                               numberOfOutputs = 1,
+                               numberOfOutputs = 2,
                                numberOfLayers = 4,
                                numberOfFiltersAtBaseLayer = 32,
                                convolutionKernelSize = c( 3, 3, 3 ),
@@ -311,7 +310,7 @@ createUnetModel3D <- function( inputImageSize,
   # Encoding path
 
   encodingConvolutionLayers <- list()
-  for( i in 1:numberOfLayers )
+  for( i in seq_len( numberOfLayers ) )
     {
     numberOfFilters <- numberOfFiltersAtBaseLayer * 2 ^ ( i - 1 )
 
@@ -380,23 +379,154 @@ createUnetModel3D <- function( inputImageSize,
     }
 
   convActivation <- ''
-  if( mode == 'classification' )
+  if( numberOfOutputs == 2 )
     {
-    if( numberOfOutputs == 2 )
-      {
-      convActivation <- 'sigmoid'
-      } else {
-      convActivation <- 'softmax'
-      }
-    } else if( mode == 'regression' ) {
-    convActivation <- 'linear'
+    convActivation <- 'sigmoid'
     } else {
-    stop( 'Error: unrecognized mode.' )
+    convActivation <- 'softmax'
     }
   outputs <- outputs %>%
     layer_conv_3d( filters = numberOfOutputs,
       kernel_size = c( 1, 1, 1 ), activation = convActivation,
       kernel_regularizer = regularizer_l2( weightDecay ) )
+
+  unetModel <- keras_model( inputs = inputs, outputs = outputs )
+
+  return( unetModel )
+}
+
+#' 3-D implementation of the "NoBrainer" U-net architecture
+#'
+#' Creates a keras model implementation of the u-net architecture
+#' avaialable here:
+#'
+#'         \url{https://github.com/neuronets/nobrainer/blob/master/nobrainer/models/unet.py}
+#'
+#' @param inputImageSize Used for specifying the input tensor shape.  The
+#' shape (or dimension) of that tensor is the image dimensions followed by
+#' the number of channels (e.g., red, green, and blue).
+#' @param numberOfOutputs Number of segmentation labels.
+#' @param numberOfFiltersAtBaseLayer number of filters at the beginning and end
+#' of the \verb{'U'}.  Doubles at each descending/ascending layer. Default = 16.
+#' @param convolutionKernelSize 3-d vector defining the kernel size
+#' during the encoding path.
+#' @param deconvolutionKernelSize 3-d vector defining the kernel size
+#' during the decoding.
+#'
+#' @return a u-net keras model
+#' @author Tustison NJ
+#' @examples
+#'
+#' # Simple examples, must run successfully and quickly. These will be tested.
+#'
+#' library( ANTsRNet )
+#' library( keras )
+#'
+#' model <- createNoBrainerUnetModel3D( c( 128, 128, 128, 1 ) )
+#' url <- "https://github.com/neuronets/nobrainer-models/releases/download/0.1/brain-extraction-unet-128iso-weights.h5"
+#' weightsFile <- "nobrainerWeights.h5"
+#' download.file( url, weightsFile )
+#' model$load_weights( weightsFile )
+#'
+#' print( model )
+#'
+#' @import keras
+#' @export
+createNoBrainerUnetModel3D <- function( inputImageSize,
+                                        numberOfOutputs = 1,
+                                        numberOfFiltersAtBaseLayer = 16,
+                                        convolutionKernelSize = c( 3, 3, 3 ),
+                                        deconvolutionKernelSize = c( 2, 2, 2 )
+                                      )
+{
+
+  inputs <- layer_input( shape = inputImageSize )
+
+  # Encoding path
+
+  outputs <- inputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 2,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+  skip1 <- outputs
+  outputs <- outputs %>% layer_max_pooling_3d( pool_size = c( 2, 2, 2 ) )
+
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 2,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 4,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+  skip2 <- outputs
+  outputs <- outputs %>% layer_max_pooling_3d( pool_size = c( 2, 2, 2 ) )
+
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 4,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 8,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+  skip3 <- outputs
+  outputs <- outputs %>% layer_max_pooling_3d( pool_size = c( 2, 2, 2 ) )
+
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 8,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 16,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+
+  # Decoding path
+
+  outputs <- outputs %>% layer_conv_3d_transpose(
+    filters = numberOfFiltersAtBaseLayer * 16,
+    kernel_size = deconvolutionKernelSize, strides = 2, padding = 'same' )
+
+  outputs <- list( skip3, outputs ) %>% layer_concatenate()
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 8,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 8,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+
+  outputs <- outputs %>% layer_conv_3d_transpose(
+    filters = numberOfFiltersAtBaseLayer * 8,
+    kernel_size = deconvolutionKernelSize, strides = 2, padding = 'same' )
+
+  outputs <- list( skip2, outputs ) %>% layer_concatenate()
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 4,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 4,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+
+  outputs <- outputs %>% layer_conv_3d_transpose(
+    filters = numberOfFiltersAtBaseLayer * 4,
+    kernel_size = deconvolutionKernelSize, strides = 2, padding = 'same' )
+
+  outputs <- list( skip1, outputs ) %>% layer_concatenate()
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 2,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+  outputs <- outputs %>% layer_conv_3d( filters = numberOfFiltersAtBaseLayer * 2,
+    kernel_size = convolutionKernelSize, padding = 'same' )
+  outputs <- outputs %>% layer_activation_relu()
+
+  convActivation <- ''
+  if( numberOfOutputs == 1 )
+    {
+    convActivation <- 'sigmoid'
+    } else {
+    convActivation <- 'softmax'
+    }
+
+  outputs <- outputs %>%
+    layer_conv_3d( filters = numberOfOutputs,
+      kernel_size = 1, activation = convActivation )
 
   unetModel <- keras_model( inputs = inputs, outputs = outputs )
 
