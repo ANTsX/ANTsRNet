004b2857820e239790a63e44f45ab5b17500e14e
Nick Tustison <ntustison@gmail.com>
2019-10-22 19:56:35 -0700

    ENH:  Add networks.

--- R/getPretrainedNetwork.R
+++ R/getPretrainedNetwork.R
@@ -30,6 +30,8 @@ getPretrainedNetwork <- function( fileId,
                  "protonLungMri",
                  "ctHumanLung",
                  "functionalLungMri",
+                 "hippMapp3rInitial",
+                 "hippMapp3rRefine",
                  "show" )
 
   if( ! fileId %in% validList )
@@ -52,7 +54,9 @@ getPretrainedNetwork <- function( fileId,
                  wholeTumorSegmentationT2Flair = "https://ndownloader.figshare.com/files/14087045",
                  protonLungMri = "https://ndownloader.figshare.com/files/13606799",
                  ctHumanLung = "https://ndownloader.figshare.com/files/16874150",
-                 functionalLungMri = "https://ndownloader.figshare.com/files/13824167"
+                 functionalLungMri = "https://ndownloader.figshare.com/files/13824167",
+                 hippMapp3rInitial = "https://ndownloader.figshare.com/files/18068408",
+                 hippMapp3rRefine = "https://ndownloader.figshare.com/files/18068411"
                  )
 
   if( missing( targetFileName ) )
