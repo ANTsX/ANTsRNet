e2a50d4fb23c3a508ab0af00484d0d74cb83ece3
muschellij2 <muschellij2@gmail.com>
2020-01-30 23:28:43 -0500

    adding in some codecov stuff - will fail due to donttest

--- R/createCycleGanModel.R
+++ R/createCycleGanModel.R
@@ -33,7 +33,7 @@
 #' keras::backend()$clear_session()
 #'
 #' ganModel <- CycleGanModel$new(
-#'    inputImageSize = inputImageSize )
+#'    inputImageSize = c( 128, 128, 3 ) )
 #'
 #' @name CycleGanModel
 NULL
