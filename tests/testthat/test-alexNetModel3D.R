70fb84f2129f98b7deb4e789b931d89509941b96
muschellij2 <muschellij2@gmail.com>
2020-01-31 14:26:31 -0500

    trying out windows - but crap no antsr

--- tests/testthat/test-alexNetModel3D.R
+++ tests/testthat/test-alexNetModel3D.R
@@ -2,7 +2,8 @@ testthat::context("AlexModels-3D")
 
 testthat::test_that("Creating 3D Models", {
   if (keras::is_keras_available()) {
-    model <- createAlexNetModel3D( inputImageSize = c(20L, 20L, 20L, 1L),
+    testthat::skip_on_travis()
+    model <- createAlexNetModel3D( inputImageSize = c(20L, 20L, 19L, 1L),
                                    numberOfClassificationLabels = 2,
                                    batch_size = 1)
     testthat::expect_is(model, "keras.engine.training.Model" )
