5a4604516255c5d83974c4d6c8586e9b2616f85f
muschellij2 <muschellij2@gmail.com>
2020-01-31 11:33:24 -0500

    try some diff memory

--- tests/testthat/test-alexNetModel3D.R
+++ tests/testthat/test-alexNetModel3D.R
@@ -1,33 +1,29 @@
-testthat::context("AlexModels-2D")
+testthat::context("AlexModels-3D")
 
-testthat::test_that("Creating 2D Models", {
+testthat::test_that("Creating 3D Models", {
   if (keras::is_keras_available()) {
-    model <- createAlexNetModel2D( inputImageSize = c(20L, 20L, 3L),
+    model <- createAlexNetModel3D( inputImageSize = c(20L, 20L, 20L, 3L),
                                    numberOfClassificationLabels = 20,
                                    mode = "regression",
-                                   batch_size = 1)
+                                   batch_size = 1 )
     testthat::expect_is(model, "keras.engine.training.Model" )
-    testthat::expect_equal(model$count_params(), 123120596L)
+    testthat::expect_equal(model$count_params(), 647945492L)
     testthat::expect_equal(length(model$weights), 16L)
     rm(model); gc(); gc()
-    Sys.sleep(1)
-    model <- createAlexNetModel2D( inputImageSize = c(20L, 20L, 3L),
+    model <- createAlexNetModel3D( inputImageSize = c(20L, 20L, 20L, 3L),
                                    numberOfClassificationLabels = 2,
                                    batch_size = 1)
     testthat::expect_is(model, "keras.engine.training.Model" )
-    testthat::expect_equal(model$count_params(), 123046850L)
+    testthat::expect_equal(model$count_params(), 647871746L)
     testthat::expect_equal(length(model$weights), 16L)
     rm(model); gc(); gc()
-    Sys.sleep(1)
-
-    model <- createAlexNetModel2D( inputImageSize = c(20L, 20L, 3L),
+    model <- createAlexNetModel3D( inputImageSize = c(20L, 20L, 20L, 3L),
                                    numberOfClassificationLabels = 3,
                                    batch_size = 1)
     testthat::expect_is(model, "keras.engine.training.Model" )
-    testthat::expect_equal(model$count_params(), 123050947L)
+    testthat::expect_equal(model$count_params(), 647875843L)
     testthat::expect_equal(length(model$weights), 16L)
     rm(model); gc(); gc()
-    Sys.sleep(1)
 
   }
 })
