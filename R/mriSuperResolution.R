6966e024cede9401b68e121190706339a7583d81
Nick Tustison <ntustison@gmail.com>
2020-08-01 17:51:36 -0700

    ENH:  Enhancements.

--- R/mriSuperResolution.R
+++ R/mriSuperResolution.R
@@ -3,6 +3,11 @@
 #' Perform super-resolution (2x) of MRI data using deep back projection network.
 #'
 #' @param image magnetic resonance image
+#' @param outputDirectory destination directory for storing the downloaded
+#' template and model weights.  Since these can be resused, if
+#' \code{is.null(outputDirectory)}, these data will be downloaded to the
+#' inst/extdata/ subfolder of the ANTsRNet package.
+#' @param verbose print progress.
 #' @return super-resolution image.
 #' @author Avants BB
 #' @examples
@@ -13,7 +18,7 @@
 #' imageSr <- mriSuperResolution( image )
 #' }
 #' @export
-mriSuperResolution <- function( image, outputDirectory = NULL )
+mriSuperResolution <- function( image, outputDirectory = NULL, verbose = FALSE )
 {
   if( image@dimension != 3 )
     {
@@ -27,12 +32,12 @@ mriSuperResolution <- function( image, outputDirectory = NULL )
       {
       cat( "MRI super-resolution:  downloading model weights.\n" )
       }
-    modelAndWeightsFileName <- getPretrainedNetwork( "mriSuperResolution", weightsFileName )
+    modelAndWeightsFileName <- getPretrainedNetwork( "mriSuperResolution", modelAndWeightsFileName )
     }
   modelSR <- load_model_hdf5( modelAndWeightsFileName )
 
-  imageSR <- applySuperResolutionModel( image, modelSR, targetRange = c( -127.5, 127.5 ) )
-  imageSR = linMatchIntensity( imageSR, resampleImageToTarget( image, imageSR ), polyOrder = 1 )
+  imageSR <- applySuperResolutionModelToImage( image, modelSR, targetRange = c( -127.5, 127.5 ) )
+  imageSR = regressionMatchImage( imageSR, resampleImageToTarget( image, imageSR ), polyOrder = 1 )
 
   return( imageSR )
 }
