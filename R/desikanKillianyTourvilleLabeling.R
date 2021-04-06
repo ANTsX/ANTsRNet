1b5e28de222d59f7b94e0762ae59b080f0cf4d7e
Nick Tustison <ntustison@gmail.com>
2021-04-06 07:03:30 -0700

    ENH:  Refactor dkt-based lobar parcellation.

--- R/desikanKillianyTourvilleLabeling.R
+++ R/desikanKillianyTourvilleLabeling.R
@@ -110,6 +110,86 @@
 #'  \item{Label 2034:}{right transverse temporal}
 #'  \item{Label 2035:}{right insula}
 #' }
+#'
+#' Performing the lobar parcellation is based on the FreeSurfer division
+#' described here:
+#'
+#'  \url{https://surfer.nmr.mgh.harvard.edu/fswiki/CorticalParcellation}
+#'
+#' \itemize{
+#'  Frontal lobe:
+#'  \item{Label 1002:}{left caudal anterior cingulate}
+#'  \item{Label 1003:}{left caudal middle frontal}
+#'  \item{Label 1012:}{left lateral orbitofrontal}
+#'  \item{Label 1014:}{left medial orbitofrontal}
+#'  \item{Label 1017:}{left paracentral}
+#'  \item{Label 1018:}{left pars opercularis}
+#'  \item{Label 1019:}{left pars orbitalis}
+#'  \item{Label 1020:}{left pars triangularis}
+#'  \item{Label 1024:}{left precentral}
+#'  \item{Label 1026:}{left rostral anterior cingulate}
+#'  \item{Label 1027:}{left rostral middle frontal}
+#'  \item{Label 1028:}{left superior frontal}
+#'  \item{Label 2002:}{right caudal anterior cingulate}
+#'  \item{Label 2003:}{right caudal middle frontal}
+#'  \item{Label 2012:}{right lateral orbitofrontal}
+#'  \item{Label 2014:}{right medial orbitofrontal}
+#'  \item{Label 2017:}{right paracentral}
+#'  \item{Label 2018:}{right pars opercularis}
+#'  \item{Label 2019:}{right pars orbitalis}
+#'  \item{Label 2020:}{right pars triangularis}
+#'  \item{Label 2024:}{right precentral}
+#'  \item{Label 2026:}{right rostral anterior cingulate}
+#'  \item{Label 2027:}{right rostral middle frontal}
+#'  \item{Label 2028:}{right superior frontal}
+#'
+#'  Parietal:
+#'  \item{Label 1008:}{left inferior parietal}
+#'  \item{Label 1010:}{left isthmus cingulate}
+#'  \item{Label 1022:}{left postcentral}
+#'  \item{Label 1023:}{left posterior cingulate}
+#'  \item{Label 1025:}{left precuneus}
+#'  \item{Label 1029:}{left superior parietal}
+#'  \item{Label 1031:}{left supramarginal}
+#'  \item{Label 2008:}{right inferior parietal}
+#'  \item{Label 2010:}{right isthmus cingulate}
+#'  \item{Label 2022:}{right postcentral}
+#'  \item{Label 2023:}{right posterior cingulate}
+#'  \item{Label 2025:}{right precuneus}
+#'  \item{Label 2029:}{right superior parietal}
+#'  \item{Label 2031:}{right supramarginal}
+#'
+#'  Temporal:
+#'  \item{Label 1006:}{left entorhinal}
+#'  \item{Label 1007:}{left fusiform}
+#'  \item{Label 1009:}{left inferior temporal}
+#'  \item{Label 1015:}{left middle temporal}
+#'  \item{Label 1016:}{left parahippocampal}
+#'  \item{Label 1030:}{left superior temporal}
+#'  \item{Label 1034:}{left transverse temporal}
+#'  \item{Label 2006:}{right entorhinal}
+#'  \item{Label 2007:}{right fusiform}
+#'  \item{Label 2009:}{right inferior temporal}
+#'  \item{Label 2015:}{right middle temporal}
+#'  \item{Label 2016:}{right parahippocampal}
+#'  \item{Label 2030:}{right superior temporal}
+#'  \item{Label 2034:}{right transverse temporal}
+#'
+#'  Occipital:
+#'  \item{Label 1005:}{left cuneus}
+#'  \item{Label 1011:}{left lateral occipital}
+#'  \item{Label 1013:}{left lingual}
+#'  \item{Label 1021:}{left pericalcarine}
+#'  \item{Label 2005:}{right cuneus}
+#'  \item{Label 2011:}{right lateral occipital}
+#'  \item{Label 2013:}{right lingual}
+#'  \item{Label 2021:}{right pericalcarine}
+#'
+#'  Other outer labels:
+#'  \item{Label 1035:}{left insula}
+#'  \item{Label 2035:}{right insula}
+#' }
+#'
 #' Preprocessing on the training data consisted of:
 #'    * n4 bias correction,
 #'    * denoising,
@@ -123,6 +203,7 @@
 #' @param doPreprocessing perform preprocessing.  See description above.
 #' @param returnProbabilityImages whether to return the two sets of probability images
 #' for the inner and outer labels.
+#' @param doLobarParcellation perform lobar parcellation (also divided by hemisphere).
 #' @param antsxnetCacheDirectory destination directory for storing the downloaded
 #' template and model weights.  Since these can be resused, if
 #' \code{is.null(antsxnetCacheDirectory)}, these data will be downloaded to the
@@ -141,7 +222,8 @@
 #' }
 #' @export
 desikanKillianyTourvilleLabeling <- function( t1, doPreprocessing = TRUE,
-  returnProbabilityImages = FALSE, antsxnetCacheDirectory = NULL, verbose = FALSE )
+  returnProbabilityImages = FALSE, doLobarParcellation = TRUE,
+  antsxnetCacheDirectory = NULL, verbose = FALSE )
 {
 
   if( t1@dimension != 3 )
@@ -356,300 +438,124 @@ desikanKillianyTourvilleLabeling <- function( t1, doPreprocessing = TRUE,
       }
     }
 
-  if( returnProbabilityImages == TRUE )
+  if( doLobarParcellation )
     {
+    if( verbose == TRUE )
+      {
+      cat( "Doing lobar parcellation.\n" )
+      }
+
+    ################################
+    #
+    # Lobar/hemisphere parcellation
+    #
+    ################################
+
+    # Consolidate lobar cortical labels
+
+    if( verbose == TRUE )
+      {
+      cat( "   Consolidating cortical labels.\n" )
+      }
+
+    frontalLabels <- c( 1002, 1003, 1012, 1014, 1017:1020, 1024, 1026:1028,
+                        2002, 2003, 2012, 2014, 2017:2020, 2024, 2026:2028 )
+    parietalLabels <- c( 1008, 1010, 1022, 1023, 1025, 1029, 1031,
+                        2008, 2010, 2022, 2023, 2025, 2029, 2031 )
+    temporalLabels <- c( 1006, 1007, 1009, 1015, 1016, 1030, 1034,
+                        2006, 2007, 2009, 2015, 2016, 2030, 2034 )
+    occipitalLabels <- c( 1005, 1011, 1013, 1021,
+                          2005, 2011, 2013, 2021 )
+
+    lobarLabels <- list( frontalLabels, parietalLabels, temporalLabels, occipitalLabels )
+
+    dktLobes <- antsImageClone( dktLabelImage )
+    dktLobes[dktLobes < 1000] <- 0
+
+    for( i in seq.int( length( lobarLabels ) ) )
+      {
+      for( j in seq.int( length( lobarLabels[[i]] ) ) )
+        {
+        dktLobes[dktLobes == lobarLabels[[i]][j]] <- i
+        }
+      }
+    dktLobes[dktLobes > length( lobarLabels )] <- 0
+
+    sixTissue <- deepAtropos( t1Preprocessed, doPreprocessing = FALSE,
+      antsxnetCacheDirectory = antsxnetCacheDirectory, verbose = verbose )
+    atroposSeg <- sixTissue$segmentationImage
+    if( doPreprocessing == TRUE )
+      {
+      atroposSeg <- antsApplyTransforms( fixed = t1, moving = atroposSeg,
+          transformlist = t1Preprocessing$templateTransforms$invtransforms,
+          whichtoinvert = c( TRUE ), interpolator = "genericLabel", verbose = verbose )
+      }
+
+    brainMask <- antsImageClone( atroposSeg )
+    brainMask[brainMask == 1 | brainMask == 5 | brainMask == 6] <- 0
+    brainMask <- thresholdImage( brainMask, 0, 0, 0, 1 )
+
+    lobarParcellation <- iMath( brainMask, "PropagateLabelsThroughMask", brainMask * dktLobes )
+
+    lobarParcellation[atroposSeg == 5] <- 5
+    lobarParcellation[atroposSeg == 6] <- 6
+
+    # Do left/right
+
+    if( verbose == TRUE )
+      {
+      cat( "   Doing left/right hemispheres.\n" )
+      }
+
+    leftLabels <- c( 4:7, 10:13, 17, 18, 25, 26, 28, 30, 91, 1002, 1003, 1005:1031, 1034, 1035 )
+    rightLabels <- c( 43:46, 49:54, 57, 58, 60, 62, 92, 2002, 2003, 2005:2031, 2034, 2035 )
+
+    hemisphereLabels <- list( leftLabels, rightLabels )
+
+    dktHemispheres <- antsImageClone( dktLabelImage )
+
+    for( i in seq.int( length( hemisphereLabels ) ) )
+      {
+      for( j in seq.int( length( hemisphereLabels[[i]] ) ) )
+        {
+        dktHemispheres[dktHemispheres == hemisphereLabels[[i]][j]] <- i
+        }
+      }
+    dktHemispheres[dktHemispheres > 2] <- 0
+
+    atroposBrainMask <- thresholdImage( atroposSeg, 0, 0, 0, 1 )
+    hemisphereParcellation <- iMath( atroposBrainMask, "PropagateLabelsThroughMask", atroposBrainMask * dktHemispheres )
+
+    for( i in seq.int( 6 ) )
+      {
+      lobarParcellation[lobarParcellation == i & hemisphereParcellation == 2] <- 6 + i
+      }
+    }
+
+  if( returnProbabilityImages == TRUE && doLobarParcellation == TRUE )
+    {
+    return( list(
+            segmentationImage = dktLabelImage,
+            lobarParcellation = lobarParcellation,
+            innerProbabilityImages = innerProbabilityImages,
+            outerProbabilityImages = outerProbabilityImages
+            )
+          )
+    } else if( returnProbabilityImages == TRUE && doLobarParcellation == FALSE ) {
     return( list(
             segmentationImage = dktLabelImage,
             innerProbabilityImages = innerProbabilityImages,
             outerProbabilityImages = outerProbabilityImages
             )
           )
+    } else if( returnProbabilityImages == FALSE && doLobarParcellation == TRUE ) {
+    return( list(
+            segmentationImage = dktLabelImage,
+            lobarParcellation = lobarParcellation
+            )
+          )
     } else {
     return( dktLabelImage )
     }
-}
-
-#' Lobar parcellation
-#'
-#' Use Desikan-Killiany-Tourville labeling to generate a lobar parcellation
-#' based on an ANTs-flavored brain mask.  The user might want to run to
-#' multiply the result by the "no-brainer" mask and/or the output from
-#' DeepAtropos.
-#'
-#' See \url{https://surfer.nmr.mgh.harvard.edu/fswiki/CorticalParcellation}
-#'
-#' \itemize{
-#'  Frontal lobe:
-#'  \item{Label 1002:}{left caudal anterior cingulate}
-#'  \item{Label 1003:}{left caudal middle frontal}
-#'  \item{Label 1012:}{left lateral orbitofrontal}
-#'  \item{Label 1014:}{left medial orbitofrontal}
-#'  \item{Label 1017:}{left paracentral}
-#'  \item{Label 1018:}{left pars opercularis}
-#'  \item{Label 1019:}{left pars orbitalis}
-#'  \item{Label 1020:}{left pars triangularis}
-#'  \item{Label 1024:}{left precentral}
-#'  \item{Label 1026:}{left rostral anterior cingulate}
-#'  \item{Label 1027:}{left rostral middle frontal}
-#'  \item{Label 1028:}{left superior frontal}
-#'  \item{Label 2002:}{right caudal anterior cingulate}
-#'  \item{Label 2003:}{right caudal middle frontal}
-#'  \item{Label 2012:}{right lateral orbitofrontal}
-#'  \item{Label 2014:}{right medial orbitofrontal}
-#'  \item{Label 2017:}{right paracentral}
-#'  \item{Label 2018:}{right pars opercularis}
-#'  \item{Label 2019:}{right pars orbitalis}
-#'  \item{Label 2020:}{right pars triangularis}
-#'  \item{Label 2024:}{right precentral}
-#'  \item{Label 2026:}{right rostral anterior cingulate}
-#'  \item{Label 2027:}{right rostral middle frontal}
-#'  \item{Label 2028:}{right superior frontal}
-#'
-#'  Parietal:
-#'  \item{Label 1008:}{left inferior parietal}
-#'  \item{Label 1010:}{left isthmus cingulate}
-#'  \item{Label 1022:}{left postcentral}
-#'  \item{Label 1023:}{left posterior cingulate}
-#'  \item{Label 1025:}{left precuneus}
-#'  \item{Label 1029:}{left superior parietal}
-#'  \item{Label 1031:}{left supramarginal}
-#'  \item{Label 2008:}{right inferior parietal}
-#'  \item{Label 2010:}{right isthmus cingulate}
-#'  \item{Label 2022:}{right postcentral}
-#'  \item{Label 2023:}{right posterior cingulate}
-#'  \item{Label 2025:}{right precuneus}
-#'  \item{Label 2029:}{right superior parietal}
-#'  \item{Label 2031:}{right supramarginal}
-#'
-#'  Temporal:
-#'  \item{Label 1006:}{left entorhinal}
-#'  \item{Label 1007:}{left fusiform}
-#'  \item{Label 1009:}{left inferior temporal}
-#'  \item{Label 1015:}{left middle temporal}
-#'  \item{Label 1016:}{left parahippocampal}
-#'  \item{Label 1030:}{left superior temporal}
-#'  \item{Label 1034:}{left transverse temporal}
-#'  \item{Label 2006:}{right entorhinal}
-#'  \item{Label 2007:}{right fusiform}
-#'  \item{Label 2009:}{right inferior temporal}
-#'  \item{Label 2015:}{right middle temporal}
-#'  \item{Label 2016:}{right parahippocampal}
-#'  \item{Label 2030:}{right superior temporal}
-#'  \item{Label 2034:}{right transverse temporal}
-#'
-#'  Occipital:
-#'  \item{Label 1005:}{left cuneus}
-#'  \item{Label 1011:}{left lateral occipital}
-#'  \item{Label 1013:}{left lingual}
-#'  \item{Label 1021:}{left pericalcarine}
-#'  \item{Label 2005:}{right cuneus}
-#'  \item{Label 2011:}{right lateral occipital}
-#'  \item{Label 2013:}{right lingual}
-#'  \item{Label 2021:}{right pericalcarine}
-#'
-#'  Other outer labels:
-#'  \item{Label 1035:}{left insula}
-#'  \item{Label 2035:}{right insula}
-#' }
-#'
-#' See \code{desikanKillianyTourvilleLabeling} for preprocessing information.
-#'
-#' @param t1 raw or preprocessed 3-D T1-weighted brain image.
-#' @param dkt dkt label image.  If NULL (default), \code{desikanKillianyTourvilleLabeling}
-#' is called.
-#' @param doPreprocessing perform preprocessing.  See description above.
-#' @param antsxnetCacheDirectory destination directory for storing the downloaded
-#' template and model weights.  Since these can be resused, if
-#' \code{is.null(antsxnetCacheDirectory)}, these data will be downloaded to the
-#' subdirectory ~/.keras/ANTsXNet/.
-#' @param verbose print progress.
-#' @return list consisting of the segmentation image and probability images for
-#' each label.
-#' @author Tustison NJ
-#' @examples
-#' \dontrun{
-#' library( ANTsRNet )
-#' library( keras )
-#'
-#' image <- antsImageRead( "t1.nii.gz" )
-#' results <- dktBasedLobarParcellation( image )
-#' }
-#' @export
-dktBasedLobarParcellation <- function( t1, dkt = NULL, doPreprocessing = TRUE,
-  antsxnetCacheDirectory = NULL, verbose = FALSE )
-{
-
-  if( t1@dimension != 3 )
-    {
-    stop( "Input image dimension must be 3." )
-    }
-
-  if( is.null( antsxnetCacheDirectory ) )
-    {
-    antsxnetCacheDirectory <- "ANTsXNet"
-    }
-
-  if( is.null( dkt ) )
-    {
-
-    ################################
-    #
-    # Preprocess image
-    #
-    ################################
-
-    t1Preprocessed <- t1
-    if( doPreprocessing == TRUE )
-      {
-      t1Preprocessing <- preprocessBrainImage( t1,
-          truncateIntensity = c( 0.01, 0.99 ),
-          doBrainExtraction = TRUE,
-          template = "croppedMni152",
-          templateTransformType = "AffineFast",
-          doBiasCorrection = TRUE,
-          doDenoising = TRUE,
-          antsxnetCacheDirectory = antsxnetCacheDirectory,
-          verbose = verbose )
-      t1Preprocessed <- t1Preprocessing$preprocessedImage * t1Preprocessing$brainMask
-      }
-
-    ################################
-    #
-    # Download spatial priors for outer model
-    #
-    ################################
-
-    if( verbose == TRUE )
-      {
-      cat( "DesikanKillianyTourville:  retrieving label spatial priors.\n" )
-      }
-    priorsFileNamePath <- getANTsXNetData( "priorDktLabels",
-      antsxnetCacheDirectory = antsxnetCacheDirectory )
-    priorsImageList <- splitNDImageToList( antsImageRead( priorsFileNamePath ) )
-
-    ################################
-    #
-    # Build outer model and load weights
-    #
-    ################################
-
-    templateSize <- c( 96L, 112L, 96L )
-    labels <- c( 0, 1002:1003, 1005:1031, 1034:1035, 2002:2003, 2005:2031, 2034:2035 )
-    channelSize <- 1 + length( priorsImageList )
-
-    unetModel <- createUnetModel3D( c( templateSize, channelSize ),
-      numberOfOutputs = length( labels ), mode = 'classification',
-      numberOfLayers = 4, numberOfFiltersAtBaseLayer = 16, dropoutRate = 0.0,
-      convolutionKernelSize = c( 3, 3, 3 ), deconvolutionKernelSize = c( 2, 2, 2 ),
-      weightDecay = 1e-5, addAttentionGating = TRUE )
-
-    if( verbose == TRUE )
-      {
-      cat( "DesikanKillianyTourville:  retrieving model weights.\n" )
-      }
-    weightsFileNamePath <- getPretrainedNetwork( "dktOuterWithSpatialPriors",
-      antsxnetCacheDirectory = antsxnetCacheDirectory )
-    load_model_weights_hdf5( unetModel, filepath = weightsFileNamePath )
-
-    unetModel %>% compile(
-      optimizer = optimizer_adam(),
-      loss = tensorflow::tf$keras$losses$CategoricalCrossentropy(),
-      metrics = c( 'accuracy', metric_categorical_crossentropy ) )
-
-    ################################
-    #
-    # Do prediction and normalize to native space
-    #
-    ################################
-
-    if( verbose == TRUE )
-      {
-      cat( "DesikanKillianyTourville:  outer model prediction.\n" )
-      }
-
-    downsampledImage <- resampleImage( t1Preprocessed, templateSize, useVoxels = TRUE, interpType = 0 )
-    imageArray <- as.array( downsampledImage )
-    imageArray <- ( imageArray - mean( imageArray ) ) / sd( imageArray )
-
-    batchX <- array( data = 0, dim = c( 1, templateSize, channelSize ) )
-    batchX[1,,,,1] <- imageArray
-
-    for( i in seq.int( length( priorsImageList ) ) )
-      {
-      priorImageArray <- as.array( resampleImage( priorsImageList[[i]], templateSize, useVoxels = TRUE, interpType = 0 ) )
-      batchX[1,,,,i+1] <- priorImageArray
-      }
-
-    predictedData <- unetModel %>% predict( batchX, verbose = verbose )
-    probabilityImagesList <- decodeUnet( predictedData, downsampledImage )
-
-    outerProbabilityImages <- list()
-    for( i in seq.int( length( probabilityImagesList[[1]] ) ) )
-      {
-      resampledImage <- resampleImage( probabilityImagesList[[1]][[i]], dim( t1Preprocessed ), useVoxels = TRUE, interpType = 0 )
-      if( doPreprocessing == TRUE )
-        {
-        outerProbabilityImages[[i]] <- antsApplyTransforms( fixed = t1, moving = resampledImage,
-            transformlist = t1Preprocessing$templateTransforms$invtransforms,
-            whichtoinvert = c( TRUE ), interpolator = "linear", verbose = verbose )
-        } else {
-        outerProbabilityImages[[i]] <- resampledImage
-        }
-      }
-
-    imageMatrix <- imageListToMatrix( outerProbabilityImages, t1 * 0 + 1 )
-    segmentationMatrix <- matrix( apply( imageMatrix, 2, which.max ), nrow = 1 )
-    segmentationImage <- matrixToImages( segmentationMatrix, t1 * 0 + 1 )[[1]]
-
-    dktLabelImage <- antsImageClone( segmentationImage )
-
-    for( i in seq.int( length( labels ) ) )
-      {
-      dktLabelImage[( segmentationImage == i )] <- labels[i]
-      }
-
-    dkt <- dktLabelImage
-    }
-
-  ################################
-  #
-  # Consolidate lobar cortical labels
-  #
-  ################################
-
-  frontalLabels <- c( 1002, 1003, 1012, 1014, 1017:1020, 1024, 1026:1028,
-                      2002, 2003, 2012, 2014, 2017:2020, 2024, 2026:2028 )
-  parietalLabels <- c( 1008, 1010, 1022, 1023, 1025, 1029, 1031,
-                       2008, 2010, 2022, 2023, 2025, 2029, 2031 )
-  temporalLabels <- c( 1006, 1007, 1009, 1015, 1016, 1030, 1034,
-                       2006, 2007, 2009, 2015, 2016, 2030, 2034 )
-  occipitalLabels <- c( 1005, 1011, 1013, 1021,
-                        2005, 2011, 2013, 2021 )
-
-  lobarLabels <- list( frontalLabels, parietalLabels, temporalLabels, occipitalLabels )
-
-  dkt[dkt < 1000] <- 0
-
-  for( i in seq.int( length( lobarLabels ) ) )
-    {
-    for( j in seq.int( length( lobarLabels[[i]] ) ) )
-      {
-      dkt[dkt == lobarLabels[[i]][j]] <- i
-      }
-    }
-  dkt[dkt > length( lobarLabels )] <- 0
-
-  atropos <- deepAtropos( t1, doPreprocessing = doPreprocessing,
-    antsxnetCacheDirectory = antsxnetCacheDirectory, verbose = verbose )
-  brainMask <- antsImageClone( atropos$segmentationImage )
-  brainMask[brainMask == 1 | brainMask == 5 | brainMask == 6] <- 0
-  brainMask <- thresholdImage( brainMask, 0, 0, 0, 1 )
-
-  lobarParcellation <- iMath( brainMask, "PropagateLabelsThroughMask", brainMask * dkt )
-
-  lobarParcellation[atropos$segmentationImage == 5] <- 5
-  lobarParcellation[atropos$segmentationImage == 6] <- 6
-
-  return( lobarParcellation )
 }
 
