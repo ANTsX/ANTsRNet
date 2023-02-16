51186c0136964348bee6cf2154ea4ade1af401bf
Nick Tustison <ntustison@gmail.com>
2023-02-15 18:54:53 -0800

    ENH:  3-D partial convolution and u-net

--- R/createCustomUnetModel.R
+++ R/createCustomUnetModel.R
@@ -817,7 +817,7 @@ createPartialConvolutionUnetModel2D <- function( inputImageSize, numberOfPriors 
       if( usePartialConv )
         {
         resampledPriorsMask <- resampledPriors %>% layer_lambda( f = function( x ) {tensorflow::tf$ones_like( x ) } )
-        mask = list( mask, resampledPriorsMask ) %>% Concatenate( axis = 3 )
+        mask = list( mask, resampledPriorsMask ) %>% layer_concatenate( axis = 3 )
         }
       }
     if( usePartialConv )
@@ -849,6 +849,184 @@ createPartialConvolutionUnetModel2D <- function( inputImageSize, numberOfPriors 
   outputs <- outputs %>%
     layer_conv_2d( filters = 1,
       kernel_size = c( 1, 1 ), activation = 'linear' )
+
+  unetModel <- keras_model( inputs = inputs, outputs = outputs )
+
+  return( unetModel )
+}
+
+
+#' 3-D implementation of the U-net architecture for inpainting using partial
+#'    convolution.
+#'
+#'         \url{https://arxiv.org/abs/1804.07723}
+#'
+#'
+#' @param inputImageSize Used for specifying the input tensor shape.  The
+#' shape (or dimension) of that tensor is the image dimensions followed by
+#' the number of channels (e.g., red, green, and blue).  The batch size
+#' (i.e., number of training images) is not specified a priori.
+#' @param numberOfPriors Specify tissue priors for use during the decoding branch.
+#' Default = 0.
+#' @param numberOfFilters vector explicitly setting the number of filters at
+#' each layer.  Defaults to number used in the paper.
+#' @param kernelSize single scalar or tuple of same length as the number of filters.
+#' Specifies the kernel size schedule for the encoding path.  Defaults to the
+#' kernel sizes used in the paper.
+#' @param usePartialConv boolean.  Testing.  Switch between vanilla convolution
+#' layers and partial convolution layers.
+#'
+#' @return a u-net keras model
+#' @author Tustison NJ
+#'
+#' @import keras
+#' @export
+createPartialConvolutionUnetModel3D <- function( inputImageSize, numberOfPriors = 0,
+  numberOfFilters = c( 64, 128, 256, 512, 512, 512, 512, 512 ),
+  kernelSize = c( 7, 5, 5, 3, 3, 3, 3, 3 ), usePartialConv = TRUE )
+{
+  if( length( kernelSize ) == 1 )
+    {
+    kernelSize <- rep( kernelSize, length( numberOfFilters ) )
+    } else if( length( kernelSize ) != length( numberOfFilters ) ) {
+    stop( "kernelSize must be a scalar or of equal length as the numberOfFilters." )
+    }
+
+  inputImage <- layer_input( shape = inputImageSize )
+  inputMask <- layer_input( shape = inputImageSize )
+
+  if( numberOfPriors > 0 )
+    {
+    inputPriors <- layer_input( shape = c( inputImageSize[1], inputImageSize[2], inputImageSize[3], numberOfPriors ) )
+    inputs <- list( inputImage, inputMask, inputPriors )
+    } else {
+    inputs <- list( inputImage, inputMask )
+    }
+
+  # Encoding path
+
+  numberOfLayers <- length( numberOfFilters )
+
+  encodingConvolutionLayers <- list()
+  pool <- NULL
+  mask <- NULL
+  for( i in seq_len( numberOfLayers ) )
+    {
+    if( i == 1 )
+      {
+      if( usePartialConv )
+        {
+        convOutputs <- inputs %>% layer_partial_conv_3d( filters = numberOfFilters[i],
+          kernelSize = kernelSize[i], padding = 'same' )
+        conv <- convOutputs[[1]]
+        mask <- convOutputs[[2]]
+        } else {
+        conv <- inputs %>% layer_conv_3d( filters = numberOfFilters[i],
+          kernel_size = kernelSize[i], padding = 'same' )
+        }
+      } else {
+        if( usePartialConv )
+          {
+          mask <- mask %>% layer_resample_tensor_3d( shape = c( dim( pool )[2], dim( pool )[3], dim( pool )[4] ),
+                                            interpolationType = 'nearestNeighbor' )
+          convOutputs <- list( pool, mask ) %>% layer_partial_conv_3d( filters = numberOfFilters[i],
+            kernelSize = kernelSize[i], padding = 'same' )
+          conv <- convOutputs[[1]]
+          mask <- convOutputs[[2]]
+          } else {
+          conv <- inputs %>% layer_conv_3d( filters = numberOfFilters[i],
+            kernel_size = kernelSize[i], padding = 'same' )
+          }
+      }
+    conv <- conv %>% layer_activation_relu()
+
+    if( usePartialConv )
+      {
+      convOutputs <- list( conv, mask ) %>% layer_partial_conv_3d( filters = numberOfFilters[i],
+        kernelSize = kernelSize[i], padding = 'same' )
+      conv <- convOutputs[[1]]
+      mask <- convOutputs[[2]]
+      } else {
+      conv <- inputs %>% layer_conv_3d( filters = numberOfFilters[i],
+        kernel_size = kernelSize[i], padding = 'same' )
+      }
+    conv <- conv %>% layer_activation_relu()
+
+    encodingConvolutionLayers[[i]] <- conv
+
+    if( i < numberOfLayers )
+      {
+      pool <- encodingConvolutionLayers[[i]] %>%
+        layer_max_pooling_3d( pool_size = c( 2, 2, 2 ), strides = c( 2, 2, 2 ) )
+      }
+    }
+
+  # Decoding path
+
+  outputs <- encodingConvolutionLayers[[numberOfLayers]]
+  for( i in 2:numberOfLayers )
+    {
+    deconv <- outputs %>%
+      layer_conv_3d_transpose( filters = numberOfFilters[numberOfLayers - i + 1],
+        kernel_size = 2,
+        padding = 'same' )
+    deconv <- deconv %>% layer_upsampling_3d( size = c( 2, 2, 2 ) )
+
+    if( usePartialConv )
+      {
+      mask <- mask %>% layer_upsampling_3d( size = c( 2, 2, 2 ) )
+      }
+    outputs <- layer_concatenate( list( deconv, encodingConvolutionLayers[[numberOfLayers - i + 1]] ), axis = 4 )
+    if( usePartialConv )
+      {
+      # mask <- list( mask, outputs ) %>% layer_lambda(
+      #   f = function( x ) { tensorflow::tf$repeat( tensorflow::tf$gather( x[[1]], list( 0L ), axis = -1L ),
+      #     tensorflow::tf$shape( x[[2]] )[5L],
+      #     axis = -1 ) } )
+      mask <- list( mask, outputs ) %>% layer_lambda(
+        f = function( x ) { tensorflow::tf$tile( tensorflow::tf$gather( x[[1]], list( 0L ), axis = -1L ),
+          list( 1L, 1L, 1L, 1L, tensorflow::tf$shape( x[[2]] )[5L] ) ) } )
+      }
+    if( numberOfPriors > 0 )
+      {
+      resampledPriors <- inputPriors %>%
+        layer_resample_tensor_3d( shape = c( dim( outputs )[2], dim( outputs )[3], dim( outputs )[4] ), interpolationType = "linear" )
+      outputs <- list( outputs, resampledPriors ) %>% layer_concatenate( axis = 4 )
+      if( usePartialConv )
+        {
+        resampledPriorsMask <- resampledPriors %>% layer_lambda( f = function( x ) { tensorflow::tf$ones_like( x ) } )
+        mask = list( mask, resampledPriorsMask ) %>% layer_concatenate( axis = 4 )
+        }
+      }
+    if( usePartialConv )
+      {
+      convOutputs <- list( outputs, mask ) %>% layer_partial_conv_3d(
+        filters = numberOfFilters[numberOfLayers - i + 1], kernelSize = 3, padding = 'same' )
+      outputs <- convOutputs[[1]]
+      mask <- convOutputs[[2]]
+      } else {
+      outputs <- outputs %>% layer_conv_3d( filters = numberOfFilters[numberOfLayers - i + 1],
+        kernel_size = 3, padding = 'same' )
+      }
+    outputs <- outputs %>% layer_activation_relu()
+
+    if( usePartialConv )
+      {
+      convOutputs <- list( outputs, mask ) %>% layer_partial_conv_3d(
+        filters = numberOfFilters[numberOfLayers - i + 1], kernelSize = 3, padding = 'same' )
+      outputs <- convOutputs[[1]]
+      mask <- convOutputs[[2]]
+      } else {
+      outputs <- outputs %>% layer_conv_3d( filters = numberOfFilters[numberOfLayers - i + 1],
+        kernel_size = 3, padding = 'same' )
+      }
+
+    outputs <- outputs %>% layer_activation_relu()
+    }
+
+  outputs <- outputs %>%
+    layer_conv_3d( filters = 1,
+      kernel_size = c( 1, 1, 1 ), activation = 'linear' )
 
   unetModel <- keras_model( inputs = inputs, outputs = outputs )
 
--- R/customConvolutionLayers.R
+++ R/customConvolutionLayers.R
@@ -299,3 +299,259 @@ layer_partial_conv_2d <- function( object, filters, kernelSize,
        )
     }
 
+
+#' Creates 3D partial convolution layer
+#'
+#' Creates 3D partial convolution layer as described in the paper
+#'
+#'   \url{https://arxiv.org/abs/1804.07723}
+#'
+#' with the implementation ported from the following python implementation
+#'
+#'   \url{https://github.com/MathiasGruber/PConv-Keras}
+#'
+#' @docType class
+#'
+#' @section Arguments:
+#'
+#' @section Details:
+#'   \code{$initialize} instantiates a new class.
+#'
+#'   \code{$call} main body.
+#'
+#'   \code{$compute_output_shape} computes the output shape.
+#'
+#' @author Tustison NJ
+#'
+#' @return a partial convolution layer 3D
+#'
+#' @name PartialConv3DLayer
+NULL
+
+#' @export
+PartialConv3DLayer <- R6::R6Class( "PartialConv3DLayer",
+
+  inherit = KerasLayer,
+
+  lock_objects = FALSE,
+
+  public = list(
+
+    dimensionality = NULL,
+
+    filters = NULL,
+
+    kernelSize = NULL,
+
+    eps = 1e-6,
+
+    strides = c( 1L, 1L, 1L ),
+
+    padding = "valid",
+
+    dataFormat = "channels_last",
+
+    dilationRate = c( 1L, 1L, 1L),
+
+    activation = NULL,
+
+    useBias = TRUE,
+
+    kernelInitializer = "glorot_uniform",
+
+    biasInitializer = "zeros",
+
+    kernelRegularizer = NULL,
+
+    biasRegularizer = NULL,
+
+    activityRegularizer = NULL,
+
+    initialize = function( filters = NULL,
+        kernelSize = NULL, eps = 1e-6, strides = c( 1L, 1L, 1L ), padding = "valid",
+        dataFormat = "channels_last", dilationRate = c( 1L, 1L, 1L ), activation = NULL,
+        useBias = TRUE, kernelInitializer = "glorot_uniform", biasInitializer = "zeros",
+        kernelRegularizer = NULL, biasRegularizer = NULL, activityRegularizer = NULL )
+      {
+      self$filters <- as.integer( filters )
+      self$kernelSize <- as.integer( kernelSize )
+      if( length( kernelSize ) == 1 )
+        {
+        self$kernelSize <- c( self$kernelSize, self$kernelSize, self$kernelSize )
+        }
+      self$eps <- eps
+      self$strides <- as.integer( strides )
+      self$padding <- padding
+      self$dataFormat <- dataFormat
+      self$dilationRate <- as.integer( dilationRate )
+      self$activation <- activation
+      self$useBias <- useBias
+      self$kernelInitializer <- kernelInitializer
+      self$biasInitializer <- biasInitializer
+      self$kernelRegularizer <- kernelRegularizer
+      self$biasRegularizer <- biasRegularizer
+      self$activityRegularizer <- activityRegularizer
+      },
+
+    build = function( input_shape )
+      {
+      if( self$dataFormat == 'channels_first' )
+        {
+        self$channelAxis <- 2
+        } else {
+        self$channelAxis <- 5
+        }
+
+      self$dimensionality <- as.list( input_shape[[1]] )[[5]]
+      # Image kernel
+      kernelShape <- list( self$kernelSize[1], self$kernelSize[2], self$kernelSize[3],
+                            self$dimensionality, self$filters )
+      self$kernel <- self$add_weight( shape = kernelShape,
+                                      initializer = self$kernelInitializer,
+                                      name = "image_kernel",
+                                      regularizer = self$kernelRegularizer,
+                                      constraint = self$kernelConstraint,
+                                      trainable = TRUE )
+      # Mask kernel
+      # self$maskKernel <- tensorflow::tf$Variable( initial_value = tensorflow::tf$ones( kernelShape ),
+      #                                             trainable = FALSE )
+      self$maskKernel <- self$add_weight( shape = kernelShape,
+                                      initializer = tensorflow::tf$keras$initializers$Ones(),
+                                      name = "mask_kernel",
+                                      trainable = FALSE )
+
+     if( self$useBias )
+       {
+       self$bias <- self$add_weight( shape = shape( self$filters ),
+                                     initializer = self$biasInitializer,
+                                     name = 'bias',
+                                     regularizer = self$biasRegularizer,
+                                     constraint = self$biasConstraint )
+       } else {
+       self$bias <- NULL
+       }
+     },
+
+   call = function( inputs, mask = NULL )
+     {
+     # Both image and mask must be supplied
+     if( ! is.list( inputs ) || length( inputs ) != 2 )
+       {
+       stop( "PartialConvolution3D must be called on a list of two tensors [img, mask]" )
+       }
+
+     features <- inputs[[1]]
+     featureMask <- inputs[[2]]
+     if( featureMask$shape[[self$channelAxis]] == 1 )
+       {
+       # featureMask <- tensorflow::tf$repeat( featureMask, featureMask$shape[[self$channelAxis]], axis=self$channelAxis )
+       featureMask <- tensorflow::tf$tile( featureMask, list( 1L, 1L, 1L, 1L, featureMask$shape[[self$channelAxis]] ) )
+       }
+
+     K <- tensorflow::tf$keras$backend
+
+     features <- tensorflow::tf$multiply( features, featureMask )
+     features <- K$conv3d( features, self$kernel, strides = self$strides, padding = "same",
+                          data_format = self$dataFormat, dilation_rate = self$dilationRate )
+     norm <- K$conv3d( featureMask, self$maskKernel, strides = self$strides, padding = 'same',
+                          data_format = self$dataFormat, dilation_rate = self$dilationRate )
+
+     # See corresponding note in antspynet as to why I opted for the following workaround.
+
+     featureMaskFanin <- self$kernelSize[1] * self$kernelSize[2] * self$kernelSize[3]
+     for( i in seq.int( 2, featureMaskFanin ) )
+       {
+       features <- tensorflow::tf$where( tensorflow::tf$equal( norm,
+                                                           tensorflow::tf$constant( i, dtype = tensorflow::tf$float32 ) ),
+                                         tensorflow::tf$math$divide( features,
+                                                           tensorflow::tf$constant( i, dtype = tensorflow::tf$float32 ) ),
+                                         features )
+       }
+
+     # Apply bias only to the image (if chosen to do so)
+     if( self$useBias )
+       {
+       features <- K$bias_add( features, self$bias, data_format = self$dataFormat )
+       }
+
+     # Apply activations on the image
+     if( ! is.null( self$activation ) )
+       {
+       features <- self$activation( features )
+       }
+
+     featureMask <- tensorflow::tf$where( tensorflow::tf$greater( norm, self$eps ), 1.0, 0.0 )
+
+     return( list( features, featureMask ) )
+     },
+
+   compute_output_shape = function( self, inputShape )
+     {
+     newSpatialDims <- rep( NA, 3 )
+     for( i in seq.int( length( newSpatialDims ) ) )
+       {
+       index <- i + 1
+       if( self$dataFormat == "channels_first" )
+         {
+         index <- i + 2
+         }
+       newSpatialDims[i] <- convOutputLength( inputShape[[index]],
+         self$kernelSize[i], self$strides[i], self$dilationRate[i], padding = "same" )
+       }
+
+     newShape <- NULL
+     if( self$data_format == "channels_first" )
+       {
+       new_shape = list( inputShape[[1]], self$filters,
+                         newSpatialDims[1], newSpatialDims[2], newSpatialDims[3] )
+       } else if( self$data_format == "channels_last" ) {
+       new_shape = list( inputShape[[1]],
+                         newSpatialDims[1], newSpatialDims[2], newSpatialDims[3],
+                         self$filters )
+       }
+     return( list( newShape, newShape ) )
+     }
+  )
+)
+
+#' Partial convolution layer 3D
+#'
+#' Creates an 3D partial convolution layer
+#'
+#' @param object Object to compose layer with. This is either a
+#' [keras::keras_model_sequential] to add the layer to,
+#' or another Layer which this layer will call.
+#' @param filters number of filters
+#' @param kernelSize kernel size
+#' @param strides strides
+#' @param padding padding
+#' @param dataFormat format
+#' @param dilationRate dilate rate
+#' @param activation activation
+#' @param kernelInitializer kernel initializer
+#' @param biasInitializer bias initializer
+#' @param kernelRegularizer kernel regularizer
+#' @param biasRegularizer bias regularizer
+#' @param activityRegularizer activity regularizer
+#' @param useBias use bias
+#' @param trainable Whether the layer weights will be updated during training.
+#' @return a keras layer tensor
+#' @author Tustison NJ
+#' @import keras
+#' @export
+layer_partial_conv_3d <- function( object, filters, kernelSize,
+    strides = c( 1L, 1L, 1L ), padding = "valid",
+    dataFormat = "channels_last", dilationRate = c( 1L, 1L, 1L ), activation = NULL,
+    kernelInitializer = "glorot_uniform", biasInitializer = "zeros",
+    kernelRegularizer = NULL, biasRegularizer = NULL, activityRegularizer = NULL,
+    useBias = TRUE, trainable = TRUE )
+    {
+    create_layer( PartialConv3DLayer, object,
+      list( filters = filters, kernelSize = kernelSize, strides = strides, padding = padding,
+            dataFormat = dataFormat, dilationRate = dilationRate, activation = activation,
+            kernelInitializer = kernelInitializer, biasInitializer = biasInitializer,
+            kernelRegularizer = kernelRegularizer, biasRegularizer = biasRegularizer,
+            activityRegularizer = activityRegularizer, useBias = useBias, trainable = trainable )
+       )
+    }
+
