44c783d0048591348c85cb0b11e18c2f73eca049
Nick Tustison <ntustison@gmail.com>
2025-02-06 10:32:41 -0800

    ENH:  Extend FOV

--- R/brainExtraction.R
+++ R/brainExtraction.R
@@ -179,9 +179,9 @@ brainExtraction <- function( image,
       reorientTemplateMask <- antsImageRead( getANTsXNetData( "hcpyaTemplateBrainMask" ) )
       reorientTemplate <- reorientTemplate * reorientTemplateMask
       reorientTemplate <- resampleImage( reorientTemplate, c( 1, 1, 1 ), useVoxels = FALSE, interpType = 0 )
-      reorientTemplate <- padOrCropImageToSize( reorientTemplate, c( 160, 176, 160 ) )
+      reorientTemplate <- padOrCropImageToSize( reorientTemplate, c( 160, 192, 160 ) )
       xfrm <- createAntsrTransform( type = "Euler3DTransform",
-        center = getCenterOfMass( reorientTemplate ), translation = c( 0, -10, -15 ) )
+        center = getCenterOfMass( reorientTemplate ), translation = c( 0, 0, 0 ) )
       reorientTemplate <- applyAntsrTransformToImage( xfrm, reorientTemplate, reorientTemplate )
       } else {
       reorientTemplate <- antsImageRead( getANTsXNetData( "S_template3" ) )
